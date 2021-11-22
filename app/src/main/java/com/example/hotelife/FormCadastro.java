8814aec4b087b49875110c58af32ef882dcf6619
Origu1 <guibatist422@hotmail.com>
2021-11-22 19:12:16 -0300

    App hotel

---
+++ app/src/main/java/com/example/hotelife/FormCadastro.java
@@ -0,0 +1,18 @@
+package com.example.hotelife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/hotelife/TelaPrincipal.java
@@ -0,0 +1,18 @@
+package com.example.hotelife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaPrincipal extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+
+        getSupportActionBar().hide();
+    }
+}
